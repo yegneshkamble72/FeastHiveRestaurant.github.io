92aea00e9585c741836ef0f117497e90d4d8bcc1
yegnesh kamble <yegnesh7219@gmail.com>
2023-12-28 21:40:45 +0530

    first commit

---
+++ js/script.js
@@ -0,0 +1,47 @@
+
+// ---------Navigation------
+
+const mobileNav = document.querySelector(".hamburger");
+const navbar = document.querySelector(".menubar");
+
+const toggleNav = () => {
+  navbar.classList.toggle("active");
+  mobileNav.classList.toggle("hamburger-active");
+};
+mobileNav.addEventListener("click", () => toggleNav());
+
+
+// --Learn More Infomation---
+
+document.getElementById("learnMoreBtn").addEventListener("click", function () {
+  var moreInfo = document.getElementById("moreInfo");
+  if (moreInfo.classList.contains("hidden")) {
+    moreInfo.classList.remove("hidden");
+    this.textContent = "Show Less";
+  } else {
+    moreInfo.classList.add("hidden");
+    this.textContent = "Learn More";
+  }
+});
+
+
+
+$(function () {
+  // ------- WOW ANIMATED ------ //
+  wow = new WOW({
+    mobile: false,
+  });
+  wow.init();
+
+  // HIDE MOBILE MENU AFTER CLIKING ON A LINK
+  $(".navbar-collapse a").click(function () {
+    $(".navbar-collapse").collapse("hide");
+  });
+
+  // NIVO LIGHTBOX
+  $("#menu a").nivoLightbox({
+    effect: "fadeScale",
+  });
+});
+
+
